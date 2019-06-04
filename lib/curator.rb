76a95153c377bebbc1903739062b866a5476c148
Sarah Tatro <statrox4@gmail.com>
2019-06-04 10:05:26 -0600

    Find artist and photo by id

--- lib/curator.rb
+++ lib/curator.rb
@@ -6,6 +6,22 @@ class Curator
   end
 
   def add_photograph(photo)
-    @photographs << photo 
+    @photographs << photo
+  end
+
+  def add_artist(artist)
+    @artists << artist
+  end
+
+  def find_artist_by_id(id)
+    @artists.find do |artist|
+      artist[:id] == id
+    end
+  end
+
+  def find_photograph_by_id(id)
+    @photographs.find do |photo|
+      photo[:id] == id
+    end
   end
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -19,6 +19,7 @@ class ArtistTest < Minitest::Test
 
   def test_it_adds_photos
     photo_1 = {
+      # binding.pry
       id: "1",
       name: "Rue Mouffetard, Paris (Boy with Bottles)",
       artist_id: "1",
@@ -35,5 +36,78 @@ class ArtistTest < Minitest::Test
     @curator.add_photograph(photo_1)
     @curator.add_photograph(photo_2)
     assert_equal [photo_1, photo_2], @curator.photographs
+    assert_equal photo_1, @curator.photographs.first
+    # assert_equal photo_1[:name], @curator.photographs.first.name
+  end
+
+  def test_it_adds_artists
+    artist_1 = {
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    }
+
+    artist_2 = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+
+    @curator.add_artist(artist_1)
+    @curator.add_artist(artist_2)
+
+    assert_equal [artist_1, artist_2], @curator.artists
+    assert_equal artist_1, @curator.artists.first
+    # assert_equal "Henri Cartier-Bresson", @curator.artists.first.name
+  end
+  
+  def test_it_finds_artist_by_id
+    artist_1 = {
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    }
+
+    artist_2 = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+
+    @curator.add_artist(artist_1)
+    @curator.add_artist(artist_2)
+
+    assert_equal artist_1, @curator.find_artist_by_id("1")
+  end
+
+  def test_it_finds_photo_by_id
+    photo_1 = {
+      # binding.pry
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    }
+
+    photo_2 = {
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    }
+
+    @curator.add_photograph(photo_1)
+    @curator.add_photograph(photo_2)
+
+    assert_equal photo_2, @curator.find_photograph_by_id("2")
+#=> #<Photograph:0x00007fd3a1801278 @artist_id="2", @id="2", @name="Moonrise, Hernandez", @year="1941">
   end
 end
