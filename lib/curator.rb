a46821e455b80d775f13a90d8e00119691470053
Sarah Tatro <statrox4@gmail.com>
2019-06-04 10:59:37 -0600

    Find artists_with_multiple_photographs

--- lib/curator.rb
+++ lib/curator.rb
@@ -38,10 +38,27 @@ class Curator
     artist_photos
   end
 
-
-
 # => artists_with_multiple_photographs -
   # => This method returns an Array of all Artists who have more than one photograph
+
+  def artists_with_multiple_photographs(artist, id)
+     multiple_photos = []
+     find_photographs_by_artist(artist).each do |photo|
+      if photo.count > 1
+        find_artist_by_id(id).each do |artist_with_id|
+           if artist_with_id[1] == id
+             multiple_photos << artist_with_id 
+           end
+        end
+      end
+      multiple_photos
+    end
+   # if find_artist_by_id(id) do |photo|
+   #    photo[:artist_id] == id
+   #  end
+    # binding.pry
+  end
+
 # => photographs_taken_by_artist_from(string) -
   # => This method takes a String representing a country.
   # => It returns an Array of Photographs that were taken by a photographer from that country.
