52e3713b7e947db4ea2ebf613ba324a7d98e4570
Sarah Tatro <statrox4@gmail.com>
2019-06-04 11:24:25 -0600

    Find photographs taken by artist from country

--- lib/curator.rb
+++ lib/curator.rb
@@ -28,6 +28,7 @@ class Curator
 # => find_photographs_by_artist(artist) -
   # => This method takes an Artist object and returns an Array of all the Artist's photographs.
   # => A Photograph is linked to an Artist by its artist_id
+
   def find_photographs_by_artist(artist)
     artist_photos = []
     @photographs.each do |photograph|
@@ -41,25 +42,34 @@ class Curator
 # => artists_with_multiple_photographs -
   # => This method returns an Array of all Artists who have more than one photograph
 
-  def artists_with_multiple_photographs(artist, id)
-     multiple_photos = []
-     find_photographs_by_artist(artist).each do |photo|
-      if photo.count > 1
-        find_artist_by_id(id).each do |artist_with_id|
-           if artist_with_id[1] == id
-             multiple_photos << artist_with_id 
-           end
-        end
-      end
-      multiple_photos
-    end
-   # if find_artist_by_id(id) do |photo|
-   #    photo[:artist_id] == id
-   #  end
-    # binding.pry
-  end
+  # def artists_with_multiple_photographs(artist, id)
+  #    multiple_photos = []
+  #    find_photographs_by_artist(artist).each do |photo|
+  #     if photo.count > 1
+  #       find_artist_by_id(id).each do |artist_with_id|
+  #          if artist_with_id[1] == id
+  #            multiple_photos << artist_with_id
+  #          end
+  #       end
+  #     end
+  #     multiple_photos
+  #   end
+  # end
 
 # => photographs_taken_by_artist_from(string) -
   # => This method takes a String representing a country.
-  # => It returns an Array of Photographs that were taken by a photographer from that country.
+  # => It returns an Array of Photographs that were taken
+  # => by a photographer from that country.
+
+  def photographs_taken_by_artist_from(country)
+    photos_by_country = []
+    @photographs.each do |photo|
+      @artists.each do |artist|
+        if photo[:artist_id] == artist[:id] && artist[:country] == country
+          photos_by_country << photo
+        end
+      end
+    end
+    photos_by_country
+  end
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -237,14 +237,73 @@ class ArtistTest < Minitest::Test
     @curator.add_artist(artist_3)
 
     diane_arbus = @curator.find_artist_by_id("3")
-#=> #<Artist:0x00007fa3c13eef88 @born="1923", @country="United States", @died="1971", @id="3", @name="Diane Arbus">
     @curator.find_photographs_by_artist(diane_arbus)
-#=> [#<Photograph:0x00007fa3c13a4050...>, #<Photograph:0x00007fa3c110e4a8...>]
-    assert_equal [artist_3], @curator.artists_with_multiple_photographs(diane_arbus, "3")
-#=> [#<Artist:0x00007fa3c13eef88 @born="1923", @country="United States", @died="1971", @id="3", @name="Diane Arbus">]
-    assert_equal 1, @curator.artists_with_multiple_photographs.length
-#=> 1
-    assert diane_arbus == @curator.artists_with_multiple_photographs.first
-#=> true
+    # assert_equal [artist_3], @curator.artists_with_multiple_photographs(diane_arbus, "3")
+    # assert_equal 1, @curator.artists_with_multiple_photographs.length
+    # assert diane_arbus == @curator.artists_with_multiple_photographs.first
+  end
+
+  def test_it_finds_photos_taken_by_artist_from_country
+    photo_1 = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    }
+
+    photo_2 = {
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    }
+
+    photo_3 = {
+      id: "3",
+      name: "Identical Twins, Roselle, New Jersey",
+      artist_id: "3",
+      year: "1967"
+    }
+
+    photo_4 = {
+      id: "4",
+      name: "Child with Toy Hand Grenade in Central Park",
+      artist_id: "3",
+      year: "1962"
+    }
+
+    artist_1 = {
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    }
+
+    artist_2 = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+
+    artist_3 = {
+      id: "3",
+      name: "Diane Arbus",
+      born: "1923",
+      died: "1971",
+      country: "United States"
+    }
+
+    @curator.add_photograph(photo_1)
+    @curator.add_photograph(photo_2)
+    @curator.add_photograph(photo_3)
+    @curator.add_photograph(photo_4)
+    @curator.add_artist(artist_1)
+    @curator.add_artist(artist_2)
+    @curator.add_artist(artist_3)
+
+    assert_equal [photo_2, photo_3, photo_4], @curator.photographs_taken_by_artist_from("United States")
   end
 end
